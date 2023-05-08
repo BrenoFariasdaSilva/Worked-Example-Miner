76c5236021bdf575bd393dd5a82ae4fa9c32a704
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-05-08 14:38:48 -0300

    Adding main function - PyDriller

--- PyDriller/main.py
+++ PyDriller/main.py
@@ -4,12 +4,19 @@ from pydriller import Repository
 # TODO: Criar o diretório antes
 # TODO: Informar o último parâmetro o diretório de saída
 
-for commit in Repository('commons-lang').traverse_commits():
-    cmd = f"java -jar ck-0.7.1-SNAPSHOT-jar-with-dependencies.jar {commit.project_path}" # TODO: Trocar o projectpath pelo SHA
-    # TODO: Olhar como ele percorre a ´arvore dos commits
-    # TODO: Variables and Metrics tentar desabilitar isso
-    # TODO: Caminho do projet FALSE 0 FALSE directory
-    process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    print(stdout.decode())
+# @brief: Main function
+# @param: None
+# @return: None
+def main():
+    for commit in Repository('commons-lang').traverse_commits():
+        cmd = f"java -jar ck-0.7.1-SNAPSHOT-jar-with-dependencies.jar {commit.project_path}" # TODO: Trocar o projectpath pelo SHA
+        # TODO: Olhar como ele percorre a ´arvore dos commits
+        # TODO: Variables and Metrics tentar desabilitar isso
+        # TODO: Caminho do projet FALSE 0 FALSE directory
+        process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        print(stdout.decode())
     
+# @brief: This function is responsible for calling the main function at the start of the program
+if __name__ == '__main__':
+    main() 
