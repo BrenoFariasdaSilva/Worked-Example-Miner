29952f41efcc43763dd38dca2a91746d25e5cdc1
Breno Farias <brenofarias@Brenos-MBP.local>
2023-06-30 09:17:29 -0300

    FEAT: Adding the top_changes which picks the codes that changed it's metrics the most

--- PyDriller/top_changes.py
+++ PyDriller/top_changes.py
@@ -1,13 +1,17 @@
 import os # for walking through directories
 import csv # for reading csv files
+import numpy as np # for calculating the min, max, avg, and third quartile of each metric
 from tqdm import tqdm # for progress bar
+
+# CONSTANTS:
+DEFAULT_CSV_FILE = "method.csv" # The default csv file name
+# method_metrics = {} # A dictionary containing the metrics of each method
 
 # @brief: Processes a csv file containing the metrics of a method
 # @param file_path: The path to the csv file
 # @param method_metrics: A dictionary containing the metrics of each method
 # @return: None
 def process_csv_file(file_path, method_metrics):
-	metrics_changes = []
 	# Open the csv file
 	with open(file_path, "r") as csvfile:
 		# Read the csv file
@@ -20,18 +24,21 @@ def process_csv_file(file_path, method_metrics):
 			cbo_modified = float(row["cboModified"])
 			wmc = float(row["wmc"])
 			rfc = float(row["rfc"])
-			
+
 			# Create a tuple containing the metrics
 			metrics = (cbo, cbo_modified, wmc, rfc)
 			method = f"{class_name} {method_name}"
-			
+
 			if method not in method_metrics: # if the method is not in the dictionary
-					metrics_changes.append(metrics) # add the metrics to the list
-					method_metrics[method] = {"metrics": metrics_changes, "changes": 1}
-			else: # if the method is in the dictionary
-					if metrics not in metrics_changes: # if the metrics are not in the list
-						metrics_changes.append(metrics) # add the metrics values to the list
-						method_metrics[method]["changes"] += 1 # increment the number of changes
+					method_metrics[method] = {"metrics": [], "changed": 0}
+
+			# Get the metrics_changes list for the method
+			metrics_changes = method_metrics[method]["metrics"]
+
+			# Try to find the same metrics in the list for the same method. If it does not exist, then add it to the list
+			if metrics not in metrics_changes: # if the metrics are not in the list
+					metrics_changes.append(metrics) # add the metrics values to the list
+					method_metrics[method]["changed"] += 1 # increment the number of changes
 
 # @brief: Traverses a directory and processes all the csv files
 # @param directory_path: The path to the directory
@@ -40,13 +47,13 @@ def traverse_directory(directory_path):
 	method_metrics = {}
 	file_count = 0
 	progress_bar = None
-	
+
 	# Traverse the directory
 	for root, dirs, files in os.walk(directory_path):
-		# Iterate through each file in the directory and call the process_csv_file function to get the methods metrics of each file in each file
+		# Iterate through each file in the directory and call the process_csv_file function to get the methods metrics of each file
 		for file in files:
 			# If the file is a csv file
-			if file == "method.csv":
+			if file == DEFAULT_CSV_FILE:
 					file_path = os.path.join(root, file) # Get the path to the csv file
 					process_csv_file(file_path, method_metrics) # Process the csv file
 					file_count += 1 # Increment the file count
@@ -54,12 +61,12 @@ def traverse_directory(directory_path):
 						progress_bar = tqdm(total=file_count) # Initialize the progress bar
 					else:
 						progress_bar.update(1) # Update the progress bar
-	
+
 	# Close the progress bar
 	if progress_bar is not None:
 		progress_bar.close()
 
-	# Return the method metrics, which is a dictionary containing the metrics of each method	
+	# Return the method metrics, which is a dictionary containing the metrics of each method  
 	return method_metrics
 
 # @brief: Gets the top changed methods
@@ -68,7 +75,7 @@ def traverse_directory(directory_path):
 # @return: A list of tuples containing the method name and the metrics
 def sort_top_changed_methods(method_metrics):
 	# Sort the methods by the number of changes
-	sorted_methods = sorted(method_metrics.items(), key=lambda x: x[1]["changes"], reverse=True)
+	sorted_methods = sorted(method_metrics.items(), key=lambda x: x[1]["changed"], reverse=True)
 	# Return the top num_methods methods
 	return sorted_methods
 
@@ -81,12 +88,12 @@ def write_top_changed_methods_to_csv(top_changed_methods):
 		# Create the csv writer
 		writer = csv.writer(csvfile)
 		# Write the header
-		writer.writerow(["Method", "Changes", "CBO", "CBO Modified", "WMC", "RFC"])
+		writer.writerow(["Method", "Changed", "CBO", "CBO Modified", "WMC", "RFC"])
 		# Write the rows
 		for method, metrics in top_changed_methods:
-			if metrics["changes"] == 1: # if the number of changes is 1, then
-				return 
-			writer.writerow([method, metrics["changes"], *metrics["metrics"]])
+			if metrics["changed"] == 1: # if the number of changes is 1, then go to the next method
+				continue
+			writer.writerow([method, metrics["changed"], *metrics["metrics"]])
 
 # @brief: The main function
 # @param: None
@@ -101,24 +108,48 @@ def main():
 		directory_path = f"ck_metrics/{repository_name}"
 
 	# Traverse the directory and get the method metrics
+	# method_metrics = traverse_directory(directory_path)
 	method_metrics = traverse_directory(directory_path)
+
+	with open("junk/method_metrics.txt", "w") as file:
+		file.write(str(method_metrics))
+
 	# Get the top changed methods
-	# print the first method, its changes, and its metrics
 	for method, metrics in method_metrics.items():
 		with open("metrics_statistics/top_changed_methods.csv", "w") as csvfile:
 			writer = csv.writer(csvfile)
-			writer.writerow(["Method", "Changes", "CBO", "CBO Modified", "WMC", "RFC"])
-			writer.writerow([method, metrics["changes"], *metrics["metrics"]])
+			writer.writerow(["Method", "Changes", "CBOMin", "CBOMax", "CBOAvg", "CBOThirdQuartile", "CBO ModifiedMin", "CBO ModifiedMax", "CBO ModifiedAvg", "CBO ModifiedThirdQuartile", "WMCMin", "WMCMax", "WMCAvg", "WMCThirdQuartile", "RFCMin", "RFCMax", "RFCAvg", "RFCThirdQuartile"])
+			# writer.writerow([method, metrics["changes"], *metrics["metrics"]])
 			# now make a loop inside the *metrics["metrics"] in order to get the min, max, avg, and third quartile of each metric (cbo, cbo_modified, wmc, rfc)
 			# then print the results in the csv file
-			for metric in metrics["metrics"]:	
-				cbo = float(min(metric[0]))
-				cbo_modified = float(max(metric[1]))
-				wmc = metric[2]
-				rfc = metric[3]
-				writer.writerow([method, metrics["changes"], cbo, cbo_modified, wmc, rfc])
+			for metric in metrics["metrics"]:
+				# check if the metrics changes is 1, then return
+				if metrics["changed"] == 1:
+					continue
+				print(f"metrics of {method}: {metric}")	
+				cboMin = float(min(metric[0]))
+				cboMax = float(max(metric[0]))
+				cboAvg = float(sum(metric[0])) / len(metric[0])
+				cboThirdQuartile = float(np.percentile(metric[0], 75))
+				cbo_modifiedMin = float(min(metric[1]))
+				cbo_modifiedMax = float(max(metric[1]))
+				cbo_modifiedAvg = float(sum(metric[1])) / len(metric[1])
+				cbo_modifiedThirdQuartile = float(np.percentile(metric[1], 75))
+				wmcMin = float(min(metric[2]))
+				wmcMax = float(max(metric[2]))
+				wmcAvg = float(sum(metric[2])) / len(metric[2])
+				wmcThirdQuartile = float(np.percentile(metric[2], 75))
+				rfcMin = float(min(metric[3]))
+				rfcMax = float(max(metric[3]))
+				rfcAvg = float(sum(metric[3])) / len(metric[3])
+				rfcThirdQuartile = float(np.percentile(metric[3], 75))
+				writer.writerow([method, metrics["changed"], cboMin, cboMax, cboAvg, cboThirdQuartile, cbo_modifiedMin, cbo_modifiedMax, cbo_modifiedAvg, cbo_modifiedThirdQuartile, wmcMin, wmcMax, wmcAvg, wmcThirdQuartile, rfcMin, rfcMax, rfcAvg, rfcThirdQuartile])
+				# cbo_modified = float(max(metric[1]))
+				# wmc = metric[2]
+				# rfc = metric[3]
+				# writer.writerow([method, metrics["changes"], cbo, cbo_modified, wmc, rfc])
 
-		break
+		# break
 	# top_changed_methods = sort_top_changed_methods(method_metrics)
 	# # Output the top changed methods
 	# write_top_changed_methods_to_csv(top_changed_methods)
