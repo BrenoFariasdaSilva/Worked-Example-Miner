a592e7ac63f7d7207b730ec2bf02023fd36d624f
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-07-04 11:24:08 -0300

    TODO: Adding TODO

--- PyDriller/specific_files_statistics.py
+++ PyDriller/specific_files_statistics.py
@@ -1,4 +1,5 @@
 # @TODO: Change the variables names and refactor to be more readable and generic (could be method or class)
+# @TODO: Comment every line of code
 
 import os # OS module in Python provides functions for interacting with the operating system
 import csv # CSV (Comma Separated Values) is a simple file format used to store tabular data, such as a spreadsheet or database
