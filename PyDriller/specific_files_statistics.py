7f3d75ca34c880fe73aacc1340b5d00ed8043b22
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-07-06 19:38:27 -0300

    REFACTOR: Renaming function in PyDriller/specific_files_statistics.py

--- PyDriller/specific_files_statistics.py
+++ PyDriller/specific_files_statistics.py
@@ -63,7 +63,7 @@ def get_repository_name_user():
 # @brief: This verifies if all the metrics are already calculated by opening the commit hashes file and checking if every commit hash in the file is a folder in the repository folder
 # @param: repository_name: Name of the repository to be analyzed
 # @return: True if all the metrics are already calculated, False otherwise
-def check_metrics_folders(repository_name):
+def check_ck_metrics_folders(repository_name):
    print(f"{backgroundColors.OKGREEN}Checking if all the {backgroundColors.OKCYAN}CK metrics{backgroundColors.OKGREEN} are already calculated for the {backgroundColors.OKCYAN}{repository_name}{backgroundColors.OKGREEN} repository...{Style.RESET_ALL}")
    current_path = PATH
    data_path = os.path.join(current_path, RELATIVE_CK_METRICS_DIRECTORY_PATH[1:]) # Join the current path with the relative path of the ck metrics directory
@@ -374,7 +374,7 @@ def main():
    repository_name = get_repository_name_user()
 
    # Check if the metrics were already calculated
-   if not check_metrics_folders(repository_name):
+   if not check_ck_metrics_folders(repository_name):
       print(f"{backgroundColors.FAIL}The metrics for {backgroundColors.OKCYAN}{repository_name}{backgroundColors.FAIL} were not calculated. Please run the main.py file first{Style.RESET_ALL}")
       return
    
