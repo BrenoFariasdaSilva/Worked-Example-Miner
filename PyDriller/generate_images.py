d50ea842c9138724258577a92b1ff08dafe67ca4
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-07-07 08:19:07 -0300

    FEAT: Adding PyDriller/generate_images.py

---
+++ PyDriller/generate_images.py
@@ -0,0 +1,39 @@
+import os # OS module in Python provides functions for interacting with the operating system
+import pandas as pd # Pandas is a fast, powerful, flexible and easy to use open source data analysis and manipulation tool,
+import matplotlib.pyplot as plt # Matplotlib is a comprehensive library for creating static, animated, and interactive visualizations in Python.
+from colorama import Style # For coloring the terminal
+
+# Import from the main.py file
+from ck_metrics import backgroundColors
+        
+# Constants:
+PATH = os.getcwd() # Get the current working directory
+
+# Changable constants:
+CLASS_CSV_FILE = "class.csv" # The name of the csv generated file from ck.
+METHOD_CSV_FILE = "method.csv" # The name of the csv generated file from ck.
+PROCESS_CLASSES = input(f"{backgroundColors.OKGREEN}Do you want to process the {backgroundColors.OKCYAN}class.csv{backgroundColors.OKGREEN} file? {backgroundColors.OKCYAN}(True/False){backgroundColors.OKGREEN}: {Style.RESET_ALL}") == "True" # If True, then process the method.csv file. If False, then process the class.csv file
+CK_CSV_FILE = CLASS_CSV_FILE if PROCESS_CLASSES else METHOD_CSV_FILE # The name of the csv generated file from ck.
+OPPOSITE_CK_CSV_FILE = METHOD_CSV_FILE if PROCESS_CLASSES else CLASS_CSV_FILE # The name of the csv generated file from ck.
+DEFAULT_REPOSITORY_NAME = "commons-lang"
+DEFAULT_CLASS_IDS = {"org.apache.commons.lang.StringUtils": "class"} # The default ids to be analyzed. It stores the class:type or method:class
+DEFAULT_METHOD_IDS = {"testBothArgsNull/0": "org.apache.commons.lang3.AnnotationUtilsTest", "suite/0": "org.apache.commons.lang.LangTestSuite"} # The default ids to be analyzed. It stores the class:type or method:class
+DEFAULT_IDS = DEFAULT_CLASS_IDS if PROCESS_CLASSES else DEFAULT_METHOD_IDS # The default ids to be analyzed. It stores the class:type or method:class
+ 
+# Relative paths:
+RELATIVE_METRICS_EVOLUTION_DIRECTORY_PATH = "/metrics_evolution"
+RELATIVE_METRICS_STATISTICS_DIRECTORY_PATH = "/metrics_statistics"
+
+# Default values:
+FULL_METRICS_EVOLUTION_DIRECTORY_PATH = PATH + RELATIVE_METRICS_EVOLUTION_DIRECTORY_PATH
+
+# @brief: Main function
+# @param: None
+# @return: None
+def main(): 
+   print(f"{backgroundColors.OKGREEN}This script {backgroundColors.OKCYAN}generates the images{backgroundColors.OKGREEN} from the {backgroundColors.OKCYAN}metrics evolution{backgroundColors.OKGREEN} of the {CK_CSV_FILE.replace('.csv', '')} of a {backgroundColors.OKCYAN}specific repository{backgroundColors.OKGREEN}.{Style.RESET_ALL}")
+   print(f"{backgroundColors.OKGREEN}The {backgroundColors.OKCYAN}source of the data{backgroundColors.OKGREEN} used to {backgroundColors.OKCYAN}generate the images{backgroundColors.OKGREEN} is the {backgroundColors.OKCYAN}{RELATIVE_METRICS_EVOLUTION_DIRECTORY_PATH}{backgroundColors.OKGREEN} directory.{Style.RESET_ALL}")
+
+# Directly run the main function if the script is executed
+if __name__ == '__main__':
+   main() # Run the main function
