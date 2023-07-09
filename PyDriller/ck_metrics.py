7fe784f24d961f5e3f915d289670c845de322c8b
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-07-09 08:55:18 -0300

    REFACTOR: Updating output_time function in PyDriller/ck_metrics.py

--- PyDriller/ck_metrics.py
+++ PyDriller/ck_metrics.py
@@ -196,14 +196,22 @@ def output_commit_progress(repository_name, commit_hash, commit_number, number_o
 # @param: time: Time to be outputted
 # @return: None
 def output_time(output_string, time):
-   if time < TIME_UNITS[0]:
-      print(f"{backgroundColors.OKGREEN}{output_string}{backgroundColors.OKCYAN}{time} seconds{Style.RESET_ALL}")
-   elif time < TIME_UNITS[1]:
-      print(f"{backgroundColors.OKGREEN}{output_string}{backgroundColors.OKCYAN}{time / TIME_UNITS[0]} minutes{Style.RESET_ALL}")
-   elif time < TIME_UNITS[2]:
-      print(f"{backgroundColors.OKGREEN}{output_string}{backgroundColors.OKCYAN}{time / TIME_UNITS[1]} hours{Style.RESET_ALL}")
+   if float(time) < int(TIME_UNITS[0]):
+      time_unit = "seconds"
+      time_value = time
+   elif float(time) < float(TIME_UNITS[1]):
+      time_unit = "minutes"
+      time_value = time / TIME_UNITS[0]
+   elif float(time) < float(TIME_UNITS[2]):
+      time_unit = "hours"
+      time_value = time / TIME_UNITS[1]
    else:
-      print(f"{backgroundColors.OKGREEN}{output_string}{backgroundColors.OKCYAN}{time / TIME_UNITS[2]} days{Style.RESET_ALL}")
+      time_unit = "days"
+      time_value = time / TIME_UNITS[2]
+
+   rounded_time = round(time_value, 2)
+   print(f"{backgroundColors.OKGREEN}{output_string}{backgroundColors.OKCYAN}{rounded_time} {time_unit}{Style.RESET_ALL}")
+
 
 # @brief: This function traverses the repository
 # @param: repository_name: Name of the repository to be analyzed
