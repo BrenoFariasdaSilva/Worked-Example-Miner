e729872148f36cde6c492ff90ffe1494aead4fe5
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-08-05 10:55:04 -0300

    REFACTOR: Updating the MINIMUM_CHANGES constant in PyDriller/metrics_changes.py

--- PyDriller/metrics_changes.py
+++ PyDriller/metrics_changes.py
@@ -14,7 +14,7 @@ from ck_metrics import backgroundColors # For coloring the terminal outputs
 # CONSTANTS:
 PATH = os.getcwd() # Get the current working directory
 PROCESS_CLASSES = input(f"{backgroundColors.OKGREEN}Do you want to process the {backgroundColors.OKCYAN}class.csv{backgroundColors.OKGREEN} file? {backgroundColors.OKCYAN}(True/False){backgroundColors.OKGREEN}: {Style.RESET_ALL}") == "True" # If True, then process the method.csv file. If False, then process the class.csv file
-MINIMUM_CHANGES = 2 # The minimum number of changes a method should have to be considered
+MINIMUM_CHANGES = 1 # The minimum number of changes a method should have to be considered
 NUMBER_OF_METRICS = 4 # The number of metrics
 DEFAULT_REPOSITORY_NAME = ["commons-lang", "jabref"] # The default repository name
 
