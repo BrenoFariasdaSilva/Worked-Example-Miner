f292defb0b4db60ce6942feccd93a32cd65e328f
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-09-16 14:21:55 -0300

    REFACTOR: Updating the generateZipFiles ShellScript

--- PyDriller/Scripts/generateZipFiles.sh
+++ PyDriller/Scripts/generateZipFiles.sh
@@ -10,11 +10,21 @@ repositories=("commons-lang" "jabref" "kafka" "zookeeper")
 # Define the list of subfolders
 subfolders=("ck_metrics" "graphics" "metrics_evolution" "metrics_predictions" "metrics_statistics" "repositories")
 
+# List of the folder to zip
+folders_list=("")
+
 # Loop through the repositories
 for repo_name in "${repositories[@]}"; do
-   echo "${subfolders[@]/%//${repo_name}}"
+   # Clean the folders_list variable
+   folders_list=("")
+   for folder in "${subfolders[@]}"; do
+      # Create a list and append the repository name to the subfolders
+      folders_list+=("../${folder}/${repo_name}/")
+      # echo "${folders_list[@]}"
+   done
+   echo "${folders_list[@]}"
    # Create a zip file for the repository
-   zip -r "${repo_name}.zip" "${subfolders[@]/%//${repo_name}}"
+   zip -r "${repo_name}.zip" "${folders_list[@]}"
 done
 
 echo "Zip files created successfully."
