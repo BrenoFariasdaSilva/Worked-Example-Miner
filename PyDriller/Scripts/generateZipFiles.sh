520284f159f585fa4ef86467a46c1dd84f80a067
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-09-16 19:17:31 -0300

    REFACTOR: Updating the generateZipFiles ShellScript Folder Creation Paths

--- PyDriller/Scripts/generateZipFiles.sh
+++ PyDriller/Scripts/generateZipFiles.sh
@@ -8,13 +8,14 @@
 repositories=("commons-lang" "jabref" "kafka" "zookeeper")
 
 # Define the list of subfolders
-subfolders=("ck_metrics" "diff" "graphics" "metrics_evolution" "metrics_predictions" "metrics_statistics" "repositories")
+subfolders=("ck_metrics" "diffs" "graphics" "metrics_evolution" "metrics_predictions" "metrics_statistics" "repositories")
 
 # List of the folder to zip
 folders_list=("")
 
 # Get the current directory
 current_dir="$(pwd)"
+save_path_prefix=""
 echo "Current directory: ${current_dir}"
 
 # If the current_dir doesn't end with "PyDriller" or "Scripts", then exit
@@ -24,13 +25,18 @@ if [[ "${current_dir}" != *"Scripts" && "${current_dir}" != *"PyDriller" ]]; the
 fi
 
 # Create a "Compressed" directory if it does not exist
-if [[ "${current_dir}" == *"Scripts" && ! -d "../compressed/" ]]; then
-   echo "Creating the compressed directory in parent path..."
-   mkdir -p "../compressed" # Create the directory
+if [[ "${current_dir}" == *"Scripts" ]]; then
+   save_path_prefix="../"
+   if [[ ! -d "${save_path_prefix}compressed/" ]]; then
+      echo "Creating the compressed directory in parent path..."
+      mkdir -p "${save_path_prefix}compressed" # Create the directory
+   fi
 fi
-if [[ "${current_dir}" == *"PyDriller" && ! -d "/compressed/" ]]; then
-   echo "Creating the compressed directory in current path..."
-   mkdir -p "compressed" # Create the directory
+if [[ "${current_dir}" == *"PyDriller" ]]; then
+   if [[ ! -d "compressed/" ]]; then
+      echo "Creating the compressed directory in current path..."
+      mkdir -p "compressed" # Create the directory
+   fi
 fi
 
 # Loop through the repositories
@@ -38,16 +44,11 @@ for repo_name in "${repositories[@]}"; do
    # Clean the folders_list variable
    folders_list=("")
    for folder in "${subfolders[@]}"; do
-      if [[ "${current_dir}" == *"Scripts" ]]; then
-         folders_list+=("../${folder}/${repo_name}/") # Add the folder to the list
-      fi
-      if [[ "${current_dir}" == *"PyDriller" ]]; then
-         folders_list+=("${folder}/${repo_name}/") # Add the folder to the list
-      fi
+      folders_list+=("${save_path_prefix}${folder}/${repo_name}/") # Add the folder to the list
    done
 
    # Create a zip file for the repository
-   zip -r "compressed/${repo_name}.zip" "${folders_list[@]}"
+   zip -r "${save_path_prefix}compressed/${repo_name}.zip" "${folders_list[@]}"
 done
 
 echo "Zip files created successfully."
