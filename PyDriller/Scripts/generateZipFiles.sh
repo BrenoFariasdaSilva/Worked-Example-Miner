e2f3b887f139ac02e226275645335774c7c4006f
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-09-16 11:05:35 -0300

    FEAT: Adding the generateZipFiles.sh

---
+++ PyDriller/Scripts/generateZipFiles.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Run:
+# chmod +x ./generateZipFiles.sh
+# ./generateZipFiles.sh
+
+# Define the list of repositories
+repositories=("commons-lang")
+
+# Define the list of subfolders
+subfolders=("ck_metrics" "graphics" "metrics_evolution" "metrics_predictions" "metrics_statistics" "repositories")
+
+# Loop through the repositories
+for repo_name in "${repositories[@]}"; do
+   echo "${subfolders[@]/%//${repo_name}}"
+   # Create a zip file for the repository
+   zip -r "${repo_name}.zip" "${subfolders[@]/%//${repo_name}}"
+done
+
+echo "Zip files created successfully."
