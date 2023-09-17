4599de0c6b9b3baa74d68699aa8447f38111d1a9
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-09-17 19:54:08 -0300

    REFACTOR: Updating the extractZipFiles ShellScript

--- PyDriller/Scripts/extractZipFiles.sh
+++ PyDriller/Scripts/extractZipFiles.sh
@@ -10,19 +10,22 @@ current_dir="$(pwd)"
 # Define the compressed directory
 compressed_dir="" 
 
-# Check if the current directory ends with "/Scripts" or "/PyDriller"
-if [[ "$current_dir" == *"/Scripts" ]]; then
-   compressed_dir="../compressed"
-elif [[ "$current_dir" == *"/PyDriller" ]]; then
-   compressed_dir="/compressed"
-else
-   echo "This script is meant to run in a directory ending with '/PyDriller' or '/PyDriller/Scripts'."
+# If the current_dir doesn't end with "/PyDriller" or "/PyDriller/Scripts", then exit
+if [[ "${current_dir}" != *"/PyDriller" && "${current_dir}" != *"/PyDriller/Scripts" ]]; then
+   echo "Please run the script from the '/PyDriller' or '/PyDriller/Scripts' directory."
    exit
 fi
 
-# if the compressed directory does not exist, then create it
+# Check if the current directory ends with "/PyDriller" or "/PyDriller/Scripts"
+if [[ "$current_dir" == *"/PyDriller" ]]; then
+   compressed_dir="/compressed"
+elif [[ "$current_dir" == *"/PyDriller/Scripts" ]]; then
+   compressed_dir="../compressed"
+fi
+
+# If the compressed directory does not exist, then create it
 if [[ ! -d "$compressed_dir" ]]; then
-   echo "Creating the compressed directory in current path..."
+   echo "Creating the ${compressed_dir} directory in current path..."
    mkdir -p "$compressed_dir" # Create the directory
 fi
 
@@ -31,11 +34,13 @@ repositories=("commons-lang" "jabref" "kafka" "zookeeper")
 
 # Loop through the specified repository names and unzip the corresponding files
 for repo_name in "${repositories[@]}"; do
-   zipfile="$compressed_dir/${repo_name}.zip"
-   if [ -e "$zipfile" ]; then
-      unzip -q "$zipfile" -d "$compressed_dir"
-      echo "Unzipped $zipfile to $compressed_dir"
-   else
+   zipfile="$compressed_dir/${repo_name}.zip" # Define the zip file path
+   if [ -e "$zipfile" ]; then # Check if the zip file exists
+      unzip -q "$zipfile" -d "$compressed_dir" # Unzip the file
+      echo "Unzipped $zipfile to $compressed_dir" 
+   else # If the zip file does not exist, then print an error message
       echo "File $zipfile not found in $compressed_dir"
    fi
 done
+
+echo "Files unzipped successfully."
