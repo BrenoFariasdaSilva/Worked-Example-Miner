30d30e7e5f11a9209ae9f70fcc149a46abc13aa2
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-09-17 20:33:08 -0300

    REFACTOR: Updating the moveExtractedFiles ShellScript

--- PyDriller/Scripts/moveExtractedFiles.sh
+++ PyDriller/Scripts/moveExtractedFiles.sh
@@ -14,6 +14,14 @@ if [[ "${current_dir}" != *"/PyDriller" && "${current_dir}" != *"/PyDriller/Scri
    exit
 fi
 
+# Define the save path prefix
+save_path_prefix=""
+
+# Check if the current directory ends with "/PyDriller" or "/PyDriller/Scripts"
+if [[ "${current_dir}" == *"/PyDriller/Scripts" ]]; then
+   save_path_prefix="../"
+fi
+
 # Define the source and destination folder names
 source_folders=("commons-lang" "jabref" "kafka" "zookeeper")
 destination_folders=("ck_metrics" "diffs" "graphics" "metrics_evolution" "metrics_predictions" "metrics_statistics" "repositories")
@@ -23,14 +31,14 @@ for source_folder in "${source_folders[@]}"; do
    # Loop through the destination folders and move the contents
    for destination_folder in "${destination_folders[@]}"; do
       # Create the destination folder if it doesn't exist
-      mkdir -p "$destination_folder"
+      mkdir -p "${save_path_prefix}${destination_folder}"
       
       # Move the contents from the source to destination folder
-      mv "$source_folder/$destination_folder"/* "$destination_folder/"
+      mv "${source_folder}/${destination_folder}"/* "${save_path_prefix}${destination_folder}/"
    done
 
    # Delete the source folder after moving its contents
-   rm -r "$source_folder"
+   rm -r "${source_folder}"
 done
 
 echo "Files moved and source folders deleted successfully."
