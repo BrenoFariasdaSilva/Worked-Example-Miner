d1b7e9e8aed4b2ad32e684ca623d0917cc3ec188
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-09-18 08:20:10 -0300

    REFACTOR: Refactoring the file_diffs.py functions

--- PyDriller/file_diffs.py
+++ PyDriller/file_diffs.py
@@ -6,6 +6,8 @@ from tqdm import tqdm # TQDM is a progress bar library with good support for nes
 from colorama import Style # Colorama is a Python library for printing colored text and stylizing terminal output.
 from ck_metrics import backgroundColors # Import the background colors from the ck_metrics module
 
+# Define the constants
+DEFAULT_REPOSITORY_NAMES = {"commons-lang":"https://github.com/apache/commons-lang", "jabref": "https://github.com/JabRef/jabref", "kafka": "https://github.com/apache/kafka", "zookeeper": "https://github.com/apache/zookeeper"}
 SOUND_COMMANDS = {"Darwin": "afplay", "Linux": "aplay", "Windows": "start"} # The sound commands for each operating system
 SOUND_FILE = "../.assets/NotificationSound.wav" # The path to the sound file
 
@@ -24,24 +26,29 @@ def play_sound():
 # Register the function to play a sound when the program finishes
 atexit.register(play_sound)
 
-# Current working directory
-cwd = os.getcwd()
+# @brief: This function loops through the repositories
+# @param: cwd - The current working directory
+# @return: None
+def loop_through_repositories(cwd):
+	for repository_name, repository_url in DEFAULT_REPOSITORY_NAMES.items():
+		# Get the commits generator and list for the repository
+		commits_generator = (commit for commit in Repository(repository_url).traverse_commits())
+		commits = list(commit for commit in Repository(repository_url).traverse_commits())
 
-# Define the default repository names and URLs
-DEFAULT_REPOSITORY_NAMES = {"commons-lang":"https://github.com/apache/commons-lang", "jabref": "https://github.com/JabRef/jabref", "kafka": "https://github.com/apache/kafka", "zookeeper": "https://github.com/apache/zookeeper"}
+		# Validate if there are enough commits to generate a diff
+		if len(commits) < 2:
+			print(f"{backgroundColors.RED}There are not enough commits in the {repository_name} repository to generate a diff.{Style.RESET_ALL}")
+			continue
 
-# Generate the diffs for each repository
-for repository_name, repository_url in DEFAULT_REPOSITORY_NAMES.items():
-	# Get the commits generator and list for the repository
-	commits_generator = (commit for commit in Repository(repository_url).traverse_commits())
-	commits = list(commit for commit in Repository(repository_url).traverse_commits())
+		# Loop through the commits of the repository and generate the diffs files
+		generate_diffs(cwd, repository_name, commits_generator, commits)
 
-	# Validate if there are enough commits to generate a diff
-	if len(commits) < 2:
-		print(f"{backgroundColors.RED}There are not enough commits in the {repository_name} repository to generate a diff.{Style.RESET_ALL}")
-		continue
-
-	# Loop through the commits of the repository and generate the diffs files
+# @brief: This function generates the diffs for the commits of a repository
+# @param: cwd - The current working directory
+# @param: repository_name - The name of the repository
+# @param: commits_generator - The generator of the commits of the repository
+# @return: None
+def generate_diffs(cwd, repository_name, commits_generator):
 	for i, commit in enumerate(tqdm(commits_generator, desc=f"{backgroundColors.GREEN}Processing {backgroundColors.CYAN}{repository_name}{backgroundColors.GREEN} commits{Style.RESET_ALL}")):
 		# Loop through the modified files of the commit
 		for modified_file in commit.modified_files:
@@ -56,4 +63,20 @@ for repository_name, repository_url in DEFAULT_REPOSITORY_NAMES.items():
 				diff_file.write(file_diff) # Write the diff to the file
 
 	print(f"{backgroundColors.GREEN}All diffs for {backgroundColors.CYAN}{repository_name}{backgroundColors.GREEN} saved successfully.{Style.RESET_ALL}\n")
-print(f"{backgroundColors.GREEN}All diffs for {backgroundColors.CYAN}{list(DEFAULT_REPOSITORY_NAMES.keys())}{backgroundColors.GREEN} saved successfully.{Style.RESET_ALL}")
+
+# @brief: This is the main function of the file_diffs module
+# @param: None
+# @return: None
+def main():
+	print(f"{backgroundColors.GREEN}Starting to generate the diffs for {backgroundColors.CYAN}{list(DEFAULT_REPOSITORY_NAMES.keys())}{backgroundColors.GREEN}.{Style.RESET_ALL}\n")
+
+	# Current working directory
+	cwd = os.getcwd()
+
+	# Loop through the repositories
+	loop_through_repositories(cwd)
+
+	print(f"{backgroundColors.GREEN}All diffs for {backgroundColors.CYAN}{list(DEFAULT_REPOSITORY_NAMES.keys())}{backgroundColors.GREEN} saved successfully.{Style.RESET_ALL}")
+
+if __name__ == "__main__":
+	main()
