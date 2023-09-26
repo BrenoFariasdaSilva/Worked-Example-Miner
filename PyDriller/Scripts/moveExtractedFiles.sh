9b27f75c87216e775b99c86e7be7c2cf9cfb337f
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-09-26 09:27:51 -0300

    REFACTOR: Updating the moveExtractedFiles ShellScript

--- PyDriller/Scripts/moveExtractedFiles.sh
+++ PyDriller/Scripts/moveExtractedFiles.sh
@@ -10,43 +10,35 @@ current_dir="$(pwd)"
 # Print the current directory
 echo "Current directory: ${current_dir}"
 
-# If the current_dir doesn't end with "/PyDriller" or "/PyDriller/Scripts", then exit
-if [[ "${current_dir}" != *"/PyDriller" && "${current_dir}" != *"/PyDriller/Scripts" ]]; then
-   echo "Please run the script from the '/PyDriller' or '/PyDriller/Scripts' directory."
-   exit
+# If the current_dir doesn't end with "/PyDriller", then exit
+if [[ "${current_dir}" != *"/PyDriller" ]]; then
+   echo "Please run the script from the '/PyDriller' with the command './Scripts/moveExtractedFiles.sh'."
+   exit # Exit the script
 fi
 
-compressed_folder_path=""
-path_prefix=""
-
-if [[ "${current_dir}" == *"/PyDriller" ]]; then
-   compressed_folder_path="/compressed" # Set the compressed directory
-elif [[ "${current_dir}" == *"/PyDriller/Scripts" ]]; then
-   compressed_folder_path="../compressed" # Set the compressed directory
-   path_prefix="../" # Set the save path prefix
-fi
+compressed_folder_path="/compressed" # Set the compressed directory
 
 # Define the extraced folder names
 extracted_folders=("ck_metrics" "diffs" "metrics_evolution" "metrics_predictions" "metrics_statistics" "repositories")
 
 # Loop through the destination folders and move the contents
-for extracted_folders in "${extracted_folders[@]}"; do
-   echo "Moving the contents from ${compressed_folder_path}/${extracted_folders} to ../"
+for extracted_folder in "${extracted_folders[@]}"; do
+   echo "Moving the contents from ${compressed_folder_path}/${extracted_folder} to ../${extracted_folder}/"
 
    # If the destination folder doesn't exist, then create it
-   if [[ ! -d "../${extracted_folders}" ]]; then
-      mkdir "../${extracted_folders}"
+   if [[ ! -d "../${extracted_folder}" ]]; then
+      mkdir "../${extracted_folder}"
    fi
    
    # Move the contents from the source to destination folder
-   mv "${compressed_folder_path}/${extracted_folders}"/* "../${extracted_folders}/"
+   mv "${compressed_folder_path}/${extracted_folder}"/* "../${extracted_folder}/"
 
    # Delete the source folder
-   rm -rf "${compressed_folder_path}/${extracted_folders}"
+   rm -rf "${compressed_folder_path}/${extracted_folder:?}"
 done
 
 # Play a sound when the script finishes
-sound_file="${path_prefix}../.assets/NotificationSound.wav"
+sound_file="../.assets/NotificationSound.wav"
 
 if [ -e "$sound_file" ]; then
   aplay "$sound_file" # Play the sound file
