14daf58053329d4b21ec9266e94fa257dc0edb1c
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-09-26 19:04:31 -0300

    FEAT: Initializing the RefactoringMiner Python

---
+++ RefactoringMiner/main.py
@@ -0,0 +1,8 @@
+import atexit # For playing a sound when the program finishes
+import os # OS module in Python provides functions for interacting with the operating system
+import platform # For getting the operating system name
+import subprocess # The subprocess module allows you to spawn new processes, connect to their input/output/error pipes, and obtain their return codes
+import threading # The threading module provides a high-level interface for running tasks in separate threads
+import time # This module provides various time-related functions
+from colorama import Style # For coloring the terminal
+from tqdm import tqdm # For Generating the Progress Bars
