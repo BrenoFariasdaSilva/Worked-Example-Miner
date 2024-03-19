3e4d5af89999cec264aa01000a502f7362294614
Breno Farias da Silva <breno.vicio78@gmail.com>
2024-03-19 19:14:29 -0300

    REFACTOR: Removing the double calculation of the found files count in the PyDriller/Track Files Python Code

--- PyDriller/Scripts/track_files.py
+++ PyDriller/Scripts/track_files.py
@@ -28,13 +28,13 @@ def search_files(search_directory, search_string):
 	:param search_directory: The directory to search in
 	:param search_string: The string to search for
 	:return: found_file_paths: A list of the paths of the found files
-	:return: file_counts: A dictionary containing the number of files found
+	:return: found_files_count: A dictionary containing the number of files found
 	"""
 
 	if VERBOSE: # If the VERBOSE constant is set to True
 		print(f"{BackgroundColors.GREEN}Searching for {BackgroundColors.CYAN}{search_string} {BackgroundColors.GREEN}files in {BackgroundColors.CYAN}{search_directory}{Style.RESET_ALL}")
 
-	file_counts = 0 # Counter for the number of files found
+	found_files_count = 0 # Counter for the number of files found
 	found_file_paths = [] # List to store the paths of the found files
 	# Walk through the directory
 	for root, _, files in os.walk(search_directory):
@@ -43,16 +43,17 @@ def search_files(search_directory, search_string):
 			file_path = os.path.join(root, file)
 			# Verify if the file name matches the target file name
 			if search_string in file:
-				file_counts += 1 # Increment the counter
+				found_files_count += 1 # Increment the counter
 				found_file_paths.append(file_path) # Add the file path to the list
 					
-	return found_file_paths, file_counts # Return the list and the dictionary
+	return found_file_paths, found_files_count # Return the list and the dictionary
 
-def write_file_paths(found_file_paths, repository_name, current_directory):
+def write_file_paths(found_file_paths, found_files_count, repository_name, current_directory):
 	"""
 	Write the found file paths to a text file.
 
 	:param found_file_paths: A list of the paths of the found files
+	:param found_files_count: A dictionary containing the number of files found
 	:param repository_name: The name of the repository
 	:param current_directory: The current directory
 	:return: None
@@ -68,7 +69,6 @@ def write_file_paths(found_file_paths, repository_name, current_directory):
 
 	# Write the found file paths to a text file
 	with open(output_file_path, "w") as file:
-		found_files_count = len(found_file_paths)
 		file.write(f"Found files in PyDriller/diffs/{repository_name}: {found_files_count}\n")
 
 		# For each file path in the list
@@ -103,7 +103,7 @@ def main():
 			continue
 
 		# Call the function to start the search
-		found_file_paths, file_counts = search_files(search_directory, TARGET_FILENAMES[repository_name])
+		found_file_paths, found_files_count = search_files(search_directory, TARGET_FILENAMES[repository_name])
 
 		print(f"{BackgroundColors.GREEN}Number of {BackgroundColors.CYAN}{TARGET_FILENAMES[repository_name]} {BackgroundColors.GREEN}files found in {BackgroundColors.CYAN}{repository_name}{BackgroundColors.GREEN}: {BackgroundColors.CYAN}{file_counts}{Style.RESET_ALL}")
 
@@ -111,7 +111,7 @@ def main():
 		found_file_paths.sort(key=lambda path: int(re.search(rf"{repository_name}/(\d+)-", path).group(1)))
 
 		# Write the file paths to a text file
-		write_file_paths(found_file_paths, repository_name, current_directory)
+		write_file_paths(found_file_paths, found_files_count, repository_name, current_directory)
  
 if __name__ == '__main__':
    """
