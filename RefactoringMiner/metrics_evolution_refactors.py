d462a5eb0320c9205fcd88ca260132a6683862e6
Breno Farias da Silva <breno.vicio78@gmail.com>
2024-03-27 14:36:31 -0300

    REFACTOR: Improving the Outputs from the RefactoringMiner/Metrics Evolution Refactors Python Code

--- RefactoringMiner/metrics_evolution_refactors.py
+++ RefactoringMiner/metrics_evolution_refactors.py
@@ -163,7 +163,8 @@ def main():
       print(f"{BackgroundColors.RED}The {BackgroundColors.CYAN}{START_PATH}{BackgroundColors.RED} constant contains whitespaces. Please remove them!{Style.RESET_ALL}")
       return
       
-   print(f"{BackgroundColors.GREEN}Welcome to the {BackgroundColors.CYAN}Metrics Evolution Refactors{BackgroundColors.GREEN}! This tool is part of the {BackgroundColors.CYAN}Worked Example Miner (WEM){BackgroundColors.GREEN} project.{Style.RESET_ALL}")
+   # Print the welcome message
+   print(f"{BackgroundColors.GREEN}Welcome to the {BackgroundColors.CYAN}Metrics Evolution Refactors{BackgroundColors.GREEN}! This script is part of the {BackgroundColors.CYAN}Worked Example Miner (WEM){BackgroundColors.GREEN} project.{Style.RESET_ALL}", end="\n\n")
    print(f"{BackgroundColors.GREEN}This Script will generate de refactors for the commits in {BackgroundColors.CYAN}{list(FILES_TO_ANALYZE.items())} {CLASSES_OR_METHODS}{BackgroundColors.GREEN} for the {BackgroundColors.CYAN}{DEFAULT_REPOSITORY}{BackgroundColors.GREEN} repository that were selected by analyzing the generated data from {BackgroundColors.CYAN}PyDriller/metrics_changes.py{BackgroundColors.GREEN} code.{Style.RESET_ALL}")
 
    # Create the json directory
@@ -173,6 +174,9 @@ def main():
 
    # Process the repository
    process_repository(DEFAULT_REPOSITORY, DEFAULT_REPOSITORIES[DEFAULT_REPOSITORY])
+
+   # Output the message that the script has finished
+   print(f"\n\n{BackgroundColors.GREEN}The {BackgroundColors.CYAN}Metrics Evolution Refactors{BackgroundColors.GREEN} script has finished!{Style.RESET_ALL}")
 
 if __name__ == '__main__':
    """
