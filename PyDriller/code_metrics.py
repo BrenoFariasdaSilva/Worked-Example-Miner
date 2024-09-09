5f86ec77a55bfbce593f97bcc7e093dfb6d6458c
Breno Farias da Silva <breno.vicio78@gmail.com>
2024-09-09 01:23:02 -0300

    REFACTOR: Updating the REPOSITORIES ATTRIBUTES Constants FilePaths from PyDriller/code_metrics.py

--- PyDriller/code_metrics.py
+++ PyDriller/code_metrics.py
@@ -60,8 +60,8 @@ RELATIVE_CK_METRICS_DIRECTORY_PATH = "/ck_metrics" # The relative path of the di
 RELATIVE_DIFFS_DIRECTORY_PATH = "/diffs" # The relative path of the directory that contains the diffs
 RELATIVE_PROGRESS_DIRECTORY_PATH = "/progress" # The relative path of the progress file
 RELATIVE_REFACTORINGS_DIRECTORY_PATH = "/refactorings" # The relative path of the directory that contains the refactorings
-RELATIVE_REPOSITORIES_ATTRIBUTES_FILE_PATH = f"/repositories_attributes{CSV_FILE_EXTENSION}" # The relative path of the file that contains the repositories attributes
 RELATIVE_REPOSITORIES_DIRECTORY_PATH = "/repositories" # The relative path of the directory that contains the repositories
+RELATIVE_REPOSITORIES_ATTRIBUTES_FILE_PATH = f"{RELATIVE_REPOSITORIES_DIRECTORY_PATH}/repositories_attributes{CSV_FILE_EXTENSION}" # The relative path of the file that contains the repositories attributes
 RELATIVE_REPOSITORIES_LIST_FILE_PATH = f"{RELATIVE_REPOSITORIES_DIRECTORY_PATH}/repositories{JSON_FILE_EXTENSION}" # The relative path of the file that contains the repositories list
 
 # Full paths (Start Path + Relative Paths):
