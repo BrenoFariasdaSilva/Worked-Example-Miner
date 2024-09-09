dec71ed5bcb8cbc0b02b453e4c05a01fc1426ca3
Breno Farias da Silva <breno.vicio78@gmail.com>
2024-09-08 22:42:10 -0300

    REFACTOR: Rouding the Size in GB Repository Attribute Value from PyDriller/code_metrics.py

--- PyDriller/code_metrics.py
+++ PyDriller/code_metrics.py
@@ -770,7 +770,7 @@ def get_repository_attributes(repository_name, number_of_commits, elapsed_time):
       "lines_of_code": total_lines_of_code,
       "commits": number_of_commits,
       "execution_time_minutes": round(elapsed_time / 60, 3),
-      "size_in_gb": output_dirs_size
+      "size_in_gb": round(output_dirs_size, 3)
    }
 
    return repository_attributes # Return the repository attributes
