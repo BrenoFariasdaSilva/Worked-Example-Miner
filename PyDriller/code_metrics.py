b9bc6788e2719360a2bfd7f4342f2e623900e191
Breno Farias <brenofarias@Brenos-MacBook-Pro.local>
2024-09-18 09:31:36 -0300

    REFACTOR: Importing the BackgroundColors from repositories_picker in PyDriller/code_metrics.py

--- PyDriller/code_metrics.py
+++ PyDriller/code_metrics.py
@@ -11,15 +11,8 @@ from colorama import Style # For coloring the terminal
 from pydriller import Repository # PyDriller is a Python framework that helps developers in analyzing Git repositories. 
 from tqdm import tqdm # For Generating the Progress Bars
 
-# Macros:
-class BackgroundColors: # Colors for the terminal
-   CYAN = "\033[96m" # Cyan
-   GREEN = "\033[92m" # Green
-   YELLOW = "\033[93m" # Yellow
-   RED = "\033[91m" # Red
-   BOLD = "\033[1m" # Bold
-   UNDERLINE = "\033[4m" # Underline
-   CLEAR_TERMINAL = "\033[H\033[J" # Clear the terminal
+# Import from the repositories_picker.py file
+from repositories_picker import BackgroundColors # For coloring the terminal outputs
 
 # Default values that can be changed:
 PROCESS_JSON_REPOSITORIES = True # Process the JSON repositories. If set to True, it will process the JSON repositories, otherwise it will pick the ones defined in the DEFAULT_REPOSITORIES dictionary.
