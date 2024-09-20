6cf99141a807143f659ea615594bd0afc7110ecf
Breno Farias <brenofarias@Brenos-MacBook-Pro.local>
2024-09-19 22:53:27 -0300

    REFACTOR: Updating the Write Metrics Track Record to TXT to Include the Code Churns Metric in PyDriller/metrics_changes.py

--- PyDriller/metrics_changes.py
+++ PyDriller/metrics_changes.py
@@ -509,6 +509,7 @@ def write_metrics_track_record_to_txt(repository_name, metrics_track_record):
 			file.write(f"\tMetrics: {value['metrics']}\n") # Write the metrics
 			file.write(f"\tCommit Hashes: {value['commit_hashes']}\n") # Write the commit hashes
 			file.write(f"\tChanged: {value['changed']}\n") # Write the changed value
+			file.write(f"\tCode Churns: {value['code_churns']}\n") # Write the code churn value
 			file.write(f"\t{'Method Invocations' if PROCESS_CLASSES else 'Methods Invoked Qty'}: {value['method_invoked']}\n") # Write the 'Method Invocations' if PROCESS_CLASS, else 'Methods Invoked Qty' value
 			file.write(f"\n") # Write a new line
 
